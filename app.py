4b903bb774c3d4316db8283e9089a5ab30629c2a
Gideon-Felt <gideon@feltslanding.com>
2020-01-20 10:29:25 -0700

    added delete route

--- app.py
+++ app.py
@@ -85,6 +85,13 @@ def update_meme(id):
     return meme_schema.jsonify(meme)
 
 # DELETE
+@app.route("/delete-meme/<id>", methods=["DELETE"])
+def delete_meme(id):
+    meme = Meme.query.get(id)
+
+    db.session.delete(meme)
+    db.session.commit()
+    return jsonify("RECORD DELETED")
 
 
 
